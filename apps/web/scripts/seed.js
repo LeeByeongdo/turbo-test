e2e81bcb45607067c801084fdb6c1816efaf6905
byeongdolee <byeongdolee@kolon.com>
2023-11-14 22:15:29 +0900

    ch6 complete

--- apps/web/scripts/seed.js
+++ apps/web/scripts/seed.js
@@ -10,6 +10,7 @@ const bcrypt = require('bcrypt');
 async function seedUsers(client) {
   try {
     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
     // Create the "invoices" table if it doesn't exist
     const createTable = await client.sql`
       CREATE TABLE IF NOT EXISTS users (
